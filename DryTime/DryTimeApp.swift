ac20ad6c9134097cb3ec7b313c9d5c24b4b76a57
ruben-tandon <A00833286@tec.mx>
2024-12-09 16:58:15 +0100

    Initial Commit

---
+++ DryTime/DryTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DryTimeApp.swift
+//  DryTime
+//
+//  Created by Rubén Kumar Tandon Ramirez on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DryTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
