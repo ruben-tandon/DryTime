99218fde9ff33c431a5a6b302de37101a3181dab
ruben-tandon <A00833286@tec.mx>
2024-12-11 14:59:02 +0100

    First implementation of search location

--- DryTime/View/LocationView.swift
+++ DryTime/View/LocationView.swift
@@ -6,11 +6,190 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationView: View {
+    @State var locationManager: LocationManager = .init()
+    @State var navigationTag: String?
+
     var body: some View {
-        Text("Choose location")
+        VStack {
+            headerView
+            searchBarView
+            contentView
+        }
+        .padding()
+        .frame(maxHeight: .infinity, alignment: .top)
+        .background {
+            NavigationLink(tag: "MAPVIEW", selection: $navigationTag) {
+                MapViewSelection()
+                    .environmentObject(locationManager)
+            } label: {
+                EmptyView()
+            }
+        }
     }
+    
+    private var headerView: some View {
+        HStack {
+            Text("Choose location")
+                .font(.title3)
+                .fontWeight(.semibold)
+        }
+    }
+
+    private var searchBarView: some View {
+        HStack(spacing: 10) {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.gray)
+            
+            TextField("Find locations here", text: $locationManager.searchText)
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal)
+        .background {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .strokeBorder(.gray)
+        }
+        .padding(.vertical, 10)
+    }
+
+    @ViewBuilder
+    private var contentView: some View {
+        if let places = locationManager.fetchedPlaces, !places.isEmpty {
+            List {
+                ForEach(places, id: \.self) { place in
+                    locationButton(for: place)
+                }
+            }
+            .listStyle(.plain)
+        } else {
+            liveLocationButton
+        }
+    }
+
+    private func locationButton(for place: CLPlacemark) -> some View {
+        Button {
+            if let coordinate = place.location?.coordinate {
+                locationManager.pickedLocation = .init(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                locationManager.mapView.region = .init(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                locationManager.addDraggablePin(coordinate: coordinate)
+                locationManager.updatePlacemark(location: .init(latitude: coordinate.latitude, longitude: coordinate.longitude))
+            }
+            navigationTag = "MAPVIEW"
+        } label: {
+            HStack(spacing: 15) {
+                Image(systemName: "mappin.circle.fill")
+                    .font(.title2)
+                    .foregroundStyle(.gray)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(place.name ?? "")
+                        .font(.body)
+                    
+                    Text(place.locality ?? "")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    }
+
+    private var liveLocationButton: some View {
+        Button {
+            if let coordinate = locationManager.userLocation?.coordinate {
+                locationManager.mapView.region = .init(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                locationManager.addDraggablePin(coordinate: coordinate)
+                locationManager.updatePlacemark(location: .init(latitude: coordinate.latitude, longitude: coordinate.longitude))
+                navigationTag = "MAPVIEW"
+            }
+        } label: {
+            Label {
+                Text("Use Current Location")
+                    .font(.callout)
+            } icon: {
+                Image(systemName: "location.north.circle.fill")
+            }
+            .foregroundColor(.green)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct MapViewSelection: View {
+    @EnvironmentObject var locationManager: LocationManager
+    var body: some View {
+        ZStack {
+            MapViewHelper()
+                .environmentObject(locationManager)
+                .ignoresSafeArea()
+            
+            if let place = locationManager.pickedPlaceMark {
+                VStack(spacing: 15) {
+                    Text("Confirm location")
+                        .font(.title2.bold())
+                    
+                    HStack(spacing: 15) {
+                        Image(systemName: "mappin.circle.fill")
+                            .font(.title2)
+                            .foregroundStyle(.gray)
+                        
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text(place.name ?? "")
+                                .font(.title3.bold())
+                            
+                            Text(place.locality ?? "")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.vertical, 10)
+                    
+                    Button {
+                        // Add confirm action here
+                    } label: {
+                        Text("Confirm location")
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 12)
+                            .background {
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .fill(.green)
+                            }
+                            .overlay(alignment: .trailing) {
+                                Image(systemName: "arrow.right")
+                                    .font(.title3.bold())
+                                    .padding(.trailing)
+                            }
+                            .foregroundColor(.white)
+                    }
+                }
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.white)
+                        .ignoresSafeArea()
+                }
+                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+        }
+        .onDisappear {
+            locationManager.pickedLocation = nil
+            locationManager.pickedPlaceMark = nil
+            locationManager.mapView.removeAnnotations(locationManager.mapView.annotations)
+        }
+    }
+}
+
+//UIKit MapView
+struct MapViewHelper: UIViewRepresentable {
+    @EnvironmentObject var locationManager: LocationManager
+    func makeUIView(context: Context) -> MKMapView {
+        return locationManager.mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {}
 }
 
 #Preview {
---
+++ DryTime/ViewModel/LocationManager.swift
@@ -0,0 +1,139 @@
+//
+//  LocationManager.swift
+//  DryTime
+//
+//  Created by Rubén Kumar Tandon Ramirez on 10/12/24.
+//
+
+import SwiftUI
+import CoreLocation
+import MapKit
+//Combine framework is imported to watch Textfield change
+import Combine
+
+class LocationManager: NSObject, ObservableObject, MKMapViewDelegate, CLLocationManagerDelegate {
+    //Properties
+    @Published var mapView: MKMapView = .init()
+    @Published var manager: CLLocationManager = .init()
+    
+    //Search bar text
+    @Published var searchText: String = ""
+    var cancellable : AnyCancellable?
+    @Published var fetchedPlaces: [CLPlacemark]?
+    
+    //User location
+    @Published var userLocation: CLLocation?
+    
+    //Final location
+    @Published var pickedLocation: CLLocation?
+    @Published var pickedPlaceMark: CLPlacemark?
+    
+    override init() {
+        super.init()
+        //Setting delegates
+        manager.delegate = self
+        mapView.delegate = self
+        
+        //Requesting location access
+        manager.requestWhenInUseAuthorization()
+        
+        //Search textfield watching
+        cancellable = $searchText
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .removeDuplicates()
+            .sink(receiveValue: { value in
+                if value != "" {
+                    self.fetchPlaces(value: value)
+                } else {
+                    self.fetchedPlaces = nil
+                }
+            })
+    }
+    
+    func fetchPlaces(value: String){
+        //Fetching places using MKLocalSearch
+        Task {
+            do {
+                let request = MKLocalSearch.Request()
+                request.naturalLanguageQuery = value.lowercased()
+                
+                let response = try await MKLocalSearch(request: request).start()
+                //We can also use Mainactor to publish changes in Main Thread
+                await MainActor.run(body: {
+                    self.fetchedPlaces = response.mapItems.compactMap({ item -> CLPlacemark? in
+                        return item.placemark
+                    })
+                })
+            } catch {
+                //Handle errors
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        //Handle errors
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.last else { return }
+        self.userLocation = currentLocation
+    }
+    
+    //Location authorization
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways: manager.requestLocation()
+        case .authorizedWhenInUse: manager.requestLocation()
+        case .denied: handleLocationError()
+        case .notDetermined: manager.requestWhenInUseAuthorization()
+        default: ()
+        }
+    }
+    
+    func handleLocationError() {
+        //Handle error
+    }
+    
+    //Add draggable pin to mapview
+    func addDraggablePin(coordinate: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Drying clothes"
+        
+        mapView.addAnnotation(annotation)
+    }
+    
+    //Enable dragging
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let marker = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Pin")
+        marker.isDraggable = true
+        marker.canShowCallout = false
+        
+        return marker
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationView.DragState, fromOldState oldState: MKAnnotationView.DragState) {
+        guard let newLocation = view.annotation?.coordinate else { return }
+        self.pickedLocation = .init(latitude: newLocation.latitude, longitude: newLocation.longitude)
+        updatePlacemark(location: .init(latitude: newLocation.latitude, longitude: newLocation.longitude))
+    }
+    
+    func updatePlacemark(location: CLLocation) {
+        Task {
+            do {
+                guard let place = try await reverseLocationCoordinates(location: location) else { return }
+                await MainActor.run(body: {
+                    self.pickedPlaceMark = place
+                })
+            } catch {
+                //Handle error
+            }
+        }
+    }
+    
+    //Displaying new location data
+    func reverseLocationCoordinates(location: CLLocation)async throws -> CLPlacemark? {
+        let place = try await CLGeocoder().reverseGeocodeLocation(location).first
+        return place
+    }
+}
